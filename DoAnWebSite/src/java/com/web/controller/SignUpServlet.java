fabd795c285ca448de1c9d639b634226c57f971e
hungnhpk00693 <hungnhpk00693@fpt.edu.vn>
2016-12-10 14:04:13 +0700

    sữa quyền lại int

---
+++ DoAnWebSite/src/java/com/web/controller/SignUpServlet.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.web.controller;
+
+import com.web.model.Users;
+import com.web.service.UserService;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author hung
+ */
+public class SignUpServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        
+       String email, fullname, username, pwd, repwd  ;
+       int quyen;
+        quyen = Integer.parseInt("quyen");
+        email = request.getParameter("email");
+        username = request.getParameter("username");
+        pwd = request.getParameter("pwd");
+        repwd = request.getParameter("repwd");
+        fullname = request.getParameter("fullname");
+        
+        
+        HttpSession sesion =request.getSession();
+        
+        if(!pwd.equals(repwd)){
+            sesion.setAttribute("Loidangnhap", "Mật khẩu không trùng nhau");
+            String url = "/register.jsp";
+            getServletContext().getRequestDispatcher(url).forward(request, response);
+        }else{
+             UserService service = new UserService();
+             Users user = new Users(username, email, pwd, quyen, fullname);
+            
+           
+            service.InsertUser(user);
+            String url = "/index.jsp";
+            getServletContext().getRequestDispatcher(url).forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
