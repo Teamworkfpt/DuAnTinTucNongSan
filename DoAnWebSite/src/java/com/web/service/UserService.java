ffa9a732f416364f94baec6106420da555b2e098
hungnhpk00693 <hungnhpk00693@fpt.edu.vn>
2016-12-10 14:04:46 +0700

    thêm tìm kiếm à lấy user

--- DoAnWebSite/src/java/com/web/service/UserService.java
+++ DoAnWebSite/src/java/com/web/service/UserService.java
@@ -7,6 +7,7 @@ package com.web.service;
 
 import com.web.hibernate.util.HibernateUtil;
 import com.web.model.Users;
+import java.util.ArrayList;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -16,7 +17,52 @@ import org.hibernate.Transaction;
  * @author hung
  */
 public class UserService {
-     public Users GetUsersByEmailOrUsersName(String username){
+     public boolean DeleteUser(int IdUser){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        try {
+            tx = session.getTransaction();
+            tx.begin();
+            Users user = new Users();
+            user.setIdUser(IdUser);
+            session.delete(user);
+            tx.commit();
+            return true;
+        } catch (Exception e) {
+            if(tx != null){
+                tx.rollback();
+            }
+            System.out.println(e.toString());
+        }finally{
+            session.close();
+        }
+        return false;
+    }
+    
+    public boolean InsertUser(Users user ){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        
+        
+       try {
+            tx = session.getTransaction();
+            tx.begin();
+            session.saveOrUpdate(user);
+            tx.commit();
+            return true;
+        } catch (Exception e) {
+            if(tx != null){
+                tx.rollback();
+            }
+            System.out.println(e.toString());
+        }finally{
+            session.close();
+        }
+        return false;
+    }
+    
+    
+    public Users GetUsersByEmailOrUsersName(String username){
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction tx = null;
         
@@ -42,13 +88,106 @@ public class UserService {
         }
             return null;
         }
-     public boolean CheckLogin(String username, String Password){
+    public boolean CheckLogin(String username, String Password){
         Users user  = GetUsersByEmailOrUsersName(username);
         if(user == null){
             return false;
         }else{
-            return user.getPwd().equals(Password);
+            if(user.getPwd().equals(Password)){
+                return true;
+                
+            }else{
+                  return false; 
+            }
         }
             
     }
+
+     public Users GetUsersByIDUsers(String userID){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        
+        
+        try {
+            tx = session.getTransaction();
+            tx.begin();
+            
+            String strQuery = "from Users where idUser = " + userID + "" ;            
+            Query query = session.createQuery(strQuery);
+            Users user = (Users) query.uniqueResult();
+            tx.commit();
+            return user;
+                    
+        } catch (Exception e) {
+              if(tx != null){
+                tx.rollback();
+            }
+            System.out.println(e.toString());
+        }finally{
+            session.close();
+        }
+            return null;
+        }
+     public int usercount = 0;
+    public ArrayList<Users> getAllUsers(int pageSize, int pageNumber){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        ArrayList listUsers = new ArrayList<Users>();
+        
+        try {
+            tx = session.getTransaction();
+            tx.begin();
+            
+            Query query = session.createQuery("from Users");
+            usercount = query.list().size();
+            query = query.setFirstResult(pageSize * (pageNumber - 1));
+            query.setMaxResults(pageSize);            
+            listUsers = (ArrayList) query.list();
+            
+            tx.commit();
+            
+        } catch (Exception e) {
+            if(tx!=null){
+                tx.rollback();
+            }
+            
+        }finally{
+            session.close();
+        }
+        
+        return listUsers;
+    }
+    public ArrayList<Users> SearchUsers(int pageSize, int pageNumber, String key){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        ArrayList listUsers = new ArrayList<Users>();
+        
+        try {
+            tx = session.getTransaction();
+            tx.begin();
+            
+            Query query = session.createQuery("from Users where UserName like '%"+key+"%'");
+                    
+            usercount = query.list().size();
+            query = query.setFirstResult(pageSize * (pageNumber - 1));
+            query.setMaxResults(pageSize);            
+            listUsers = (ArrayList) query.list();
+            
+            tx.commit();
+            
+        } catch (Exception e) {
+            if(tx!=null){
+                tx.rollback();
+            }
+            
+        }finally{
+            session.close();
+        }
+        
+        return listUsers;
+    }
 }
+
+    
+        
+
