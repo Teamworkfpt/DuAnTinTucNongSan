2324cb08ef8f54cf00994c99680aab67985bb9ea
thangnxpk00638 <thangnxpk00638@fpt.edu.vn>
2016-12-14 14:25:42 +0700

    sữa lại cơ sỡ dữ liệu

--- Database/db_qltp.sql
+++ Database/db_qltp.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
 -- Host: localhost    Database: db_qltp
 -- ------------------------------------------------------
--- Server version	5.7.13-log
+-- Server version	5.7.16-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,6 +14,59 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `quanlitintuc`
+--
+
+DROP TABLE IF EXISTS `quanlitintuc`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `quanlitintuc` (
+  `IDTinTuc` int(11) NOT NULL,
+  `MaTin` int(11) NOT NULL,
+  `TieuDe` varchar(200) NOT NULL,
+  `BaiDang` text NOT NULL,
+  `mota` text NOT NULL,
+  `NgayDang` time NOT NULL,
+  PRIMARY KEY (`IDTinTuc`),
+  UNIQUE KEY `IDTinTuc_UNIQUE` (`IDTinTuc`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `quanlitintuc`
+--
+
+LOCK TABLES `quanlitintuc` WRITE;
+/*!40000 ALTER TABLE `quanlitintuc` DISABLE KEYS */;
+/*!40000 ALTER TABLE `quanlitintuc` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sanpham`
+--
+
+DROP TABLE IF EXISTS `sanpham`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sanpham` (
+  `idSanPham` int(11) NOT NULL,
+  `TenSanPham` varchar(45) NOT NULL,
+  `MoTa` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idSanPham`),
+  UNIQUE KEY `idSanPham_UNIQUE` (`idSanPham`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sanpham`
+--
+
+LOCK TABLES `sanpham` WRITE;
+/*!40000 ALTER TABLE `sanpham` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sanpham` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -45,7 +98,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'danhtp','danhtp@gmail.com','12345',0,'Phước Danh','1992-08-20 00:00:00','\0',NULL),(2,'tunx','tunx@gmail.com','12345',0,'Xuân Tú','2016-10-19 18:41:29','',NULL),(5,'tuannc','timeless194@gmail.com','123123',0,'Nguyễn Chí Tuấn','2016-10-19 19:03:30','',NULL);
+INSERT INTO `users` VALUES (1,'danhtp','danhtp@gmail.com','12345',1,'Phước Danh','1992-08-20 00:00:00','\0',NULL),(2,'tunx','tunx@gmail.com','12345',0,'Xuân Tú','2016-10-19 18:41:29','',NULL),(5,'tuannc','timeless194@gmail.com','123123',1,'Nguyễn Chí Tuấn','2016-10-19 19:03:30','',NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -58,4 +111,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-11-19  9:32:09
+-- Dump completed on 2016-12-14 14:23:45
