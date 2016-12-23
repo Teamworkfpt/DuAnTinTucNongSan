5acbe5650b237c7e51336ce688741414a19653b2
hungnhpk00693 <hungnhpk00693@fpt.edu.vn>
2016-12-23 10:57:01 +0700

    chỉnh sữa thêm thông tin sevlet

--- DoAnWebSite/src/java/com/web/controller/sigupsevlet.java
+++ DoAnWebSite/src/java/com/web/controller/sigupsevlet.java
@@ -33,8 +33,6 @@ public class sigupsevlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        
-        response.setContentType("text/html;charset=UTF-8");
         request.setCharacterEncoding("UTF-8");
         
         
