e819ae6e788da51b1257b18669a6d763582f4148
thangnxpk00638 <thangnxpk00638@fpt.edu.vn>
2016-12-23 20:50:46 +0700

    sua lai Hibernate

--- DoAnWebSite/src/java/com/web/service/QuanLiTinTucService.java
+++ DoAnWebSite/src/java/com/web/service/QuanLiTinTucService.java
@@ -7,7 +7,6 @@ package com.web.service;
 
 import com.web.hibernate.util.HibernateUtil;
 import com.web.model.Quanlitintuc;
-import com.web.model.Users;
 import java.util.ArrayList;
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -46,6 +45,48 @@ public class QuanLiTinTucService {
         }
         
         return listNews;
+    }
+      public ArrayList<Quanlitintuc> GetNews(){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        ArrayList<Quanlitintuc> listNews = new ArrayList<>();
+        try{
+            tx = session.getTransaction();
+            tx.begin();
+            Query query = session.createQuery("from Quanlitintuc");
+            listNews = (ArrayList) query.list();
+        }catch(Exception e){
+            if(tx!=null){
+                tx.rollback();
+            }
+            System.out.println("Loi" + e.toString());
+            
+        }finally{
+            session.close();
+        }
+        return  listNews;
+    }
+       public Quanlitintuc GetNewsByID(String Matin){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        try{
+            tx = session.getTransaction();
+            tx.begin();
+            String stringQuery = "from Quanlitintuc where Matin ='" + Matin + "'";
+            Query query = session.createQuery(stringQuery);
+            Quanlitintuc Qltt =  (Quanlitintuc) query.uniqueResult();
+            tx.commit();
+            return Qltt;
+        }catch (Exception e){
+            if(tx != null){
+                tx.rollback();
+            }
+            System.out.println(e.toString());
+        }finally{
+            session.close();
+        }
+        return null;
+
     }
      public ArrayList<Quanlitintuc> SearchNews(int pageSize, int pageNumber, String key){
         Session session = HibernateUtil.getSessionFactory().openSession();
